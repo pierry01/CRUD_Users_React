d91fe199a3a7ab8c43f9a22636593f97aa5a9b3f
Jean Mendonça <pierrybm@gmail.com>
2020-10-08 23:52:35 -0300

    set states, effect and onSubmit

--- src/Home/Form/EditUserForm.js
+++ src/Home/Form/EditUserForm.js
@@ -1,13 +1,28 @@
 import React, { useState, useEffect } from "react";
+
 import { Container } from "./styles";
+
 import { useForm } from "react-hook-form";
+
 import { Button, TextField } from "@material-ui/core";
 import ClearIcon from "@material-ui/icons/Clear";
 import CheckIcon from "@material-ui/icons/Check";
 
-import { Container } from "./styles";
-
 const Form = () => {
+  const { register, handleSubmit, errors } = useForm();
+  const [user, setUser] = useState(props.currentUser);
+
+  useEffect(() => {
+    setUser(props.currentUser);
+  }, [props]);
+
+  const onSubmit = (updatedUser) => {
+    props.updateUser(user.id, updatedUser);
+    window.location.reload();
+  };
+
+  const validarCpf = require("validar-cpf");
+
   return (
     <Container>
       <h1>Form</h1>
