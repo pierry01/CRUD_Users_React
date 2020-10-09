344e3c0dbacb01ae0e8a1a7593fc97aa1a901226
Jean Mendonça <pierrybm@gmail.com>
2020-10-08 23:51:21 -0300

    add necessary imports

--- src/Home/Form/EditUserForm.js
+++ src/Home/Form/EditUserForm.js
@@ -1,4 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Container } from "./styles";
+import { useForm } from "react-hook-form";
+import { Button, TextField } from "@material-ui/core";
+import ClearIcon from "@material-ui/icons/Clear";
+import CheckIcon from "@material-ui/icons/Check";
 
 import { Container } from "./styles";
 
